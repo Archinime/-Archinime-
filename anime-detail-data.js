b56097eaab4dcba7284d8c55db39478e0d85b92d
Archinime <archinime12@gmail.com>
2026-01-07 22:03:07 -0500

    Update anime-detail-data.js via CMS

--- anime-detail-data.js
+++ anime-detail-data.js
@@ -3756,5 +3756,22 @@
 
 
 
-    };
-
+    
+    72: {
+        title: "Prueba",
+        desc: "",
+        cover: "https://www.dropbox.com/scl/fi/xhgst0hwk6s5dsrfyz1zs/synt.avif?rlkey=c0agosckce8dsmorhk08rfly4&st=wd5pkyj1&raw=1",
+        seasons: [
+          {
+            num: 1,
+            name: "Temporada 1",
+            cover: "https://www.dropbox.com/scl/fi/xhgst0hwk6s5dsrfyz1zs/synt.avif?rlkey=c0agosckce8dsmorhk08rfly4&st=wd5pkyj1&raw=1",
+            eps: [
+              { title: "Capítulo 1"},
+              { title: "Capítulo 2"},
+              { title: "Capítulo 3"},
+            ]
+          },
+        ]
+    },
+};
