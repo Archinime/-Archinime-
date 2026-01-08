307ed84dbaecc9dc20539f9ef351e5b8d5fd98bd
Archinime <archinime12@gmail.com>
2026-01-08 10:57:57 -0500

    Update musica-data.js via CMS

--- musica-data.js
+++ musica-data.js
@@ -218,5 +218,9 @@
         70: [ // Kimi to Koete Koi ni Naru
             "kimitokoete.mp3"
         ],
-        71: ["overlord.mp3"] 
-    };
+        71: ["overlord.mp3"],
+        72: [
+            "https://www.dropbox.com/scl/fi/v7244hmfgzqg0ode1bsho/syn2.mp3?rlkey=6vyzi3qr8ci8hnzncxayrhez1&st=ee75uqat&raw=1",
+            "https://www.dropbox.com/scl/fi/7lp1tkbioyyi3bj9mhq8y/TICK-TICK-TICK-TICK-TICK-BITE-Archinime.mp3?rlkey=vsg0gby24ceyakz88tnh7gt3f&st=ya5pf5ow&raw=1"
+        ]
+};
