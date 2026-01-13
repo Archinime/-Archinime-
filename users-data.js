d9bbef7f47215aefd2d5cd61a61537c8f7348613
LucioLucio935 <lucioguapofeo@gmail.com>
2026-01-12 21:11:20 -0500

    Update users-data.js via CMS (New)

--- users-data.js
+++ users-data.js
@@ -10,7 +10,7 @@ const usersData = {
         "social": ""
     },
     "lucioguapofeo@gmail.com": {
-        "nick": "lulupro",
+        "nick": "lulupro123",
         "avatar": "https://avatars.githubusercontent.com/u/254288605?v=4",
         "social": "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQfBPzBsxbKWHjJHjwRqqQVGQzq"
     }
