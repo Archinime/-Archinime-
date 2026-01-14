203900611be5e901e1cf5b02a2e43bb12e39f61a
adrielchimoy252-pixel <adrielchimoy252@gmail.com>
2026-01-14 11:39:15 -0500

    Update users-data.js via CMS (New)

--- users-data.js
+++ users-data.js
@@ -13,5 +13,10 @@ const usersData = {
         "nick": "lululove12345",
         "avatar": "https://avatars.githubusercontent.com/u/254288605?v=4",
         "social": "https://gemini.google.com/app/5c9720244f5cf225?is_sa=1&is_sa=1&android-min-version=301356232&ios-min-version=322.0&campaign_id=bkws&utm_source=sem&utm_medium=paid-media&utm_campaign=bkws&pt=9008&mt=8&ct=p-growth-sem-bkws&gclsrc=aw.ds&gad_source=1&gad_campaignid=21591094075&gbraid=0AAAAApk5BhmHoDpYBgNRSSVIdC-RTvtfE&gclid=Cj0KCQiAvOjKBhC9ARIsAFvz5lhfk13LRRMsTKsys5NciCiSS2uh8e8F1zdT-gqaIxjYefILR6tRPE8aAm_dEALw_wcB"
+    },
+    "adrielchimoy252@gmail.com": {
+        "nick": "E-Sul2007",
+        "avatar": "https://avatars.githubusercontent.com/u/254942609?v=4",
+        "social": ""
     }
 };
