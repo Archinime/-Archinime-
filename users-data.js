a46be2b454781a09229a66b8d0b3dc0e40fa2133
LucioLucio935 <lucioguapofeo@gmail.com>
2026-01-13 20:41:05 -0500

    Update users-data.js via CMS (New)

--- users-data.js
+++ users-data.js
@@ -10,7 +10,7 @@ const usersData = {
         "social": ""
     },
     "lucioguapofeo@gmail.com": {
-        "nick": "lululove",
+        "nick": "lululove123",
         "avatar": "https://avatars.githubusercontent.com/u/254288605?v=4",
         "social": "https://gemini.google.com/app/5c9720244f5cf225?is_sa=1&is_sa=1&android-min-version=301356232&ios-min-version=322.0&campaign_id=bkws&utm_source=sem&utm_medium=paid-media&utm_campaign=bkws&pt=9008&mt=8&ct=p-growth-sem-bkws&gclsrc=aw.ds&gad_source=1&gad_campaignid=21591094075&gbraid=0AAAAApk5BhmHoDpYBgNRSSVIdC-RTvtfE&gclid=Cj0KCQiAvOjKBhC9ARIsAFvz5lhfk13LRRMsTKsys5NciCiSS2uh8e8F1zdT-gqaIxjYefILR6tRPE8aAm_dEALw_wcB"
     }
