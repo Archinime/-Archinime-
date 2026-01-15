9a14e9fa2fe6d95cb9d1469c57b8ac0db7cb985b
Archinime <archinime12@gmail.com>
2026-01-15 18:53:48 -0500

    Add files via upload

---
+++ musica_fondo.js
@@ -0,0 +1,14 @@
+/* Archivo: musica_fondo.js 
+   Lista de reproducción de música de fondo */
+
+const musicList = [
+    'Nightcore - How Do You Do (Remix) ✕.mp3',
+    'Nightcore - Battlecry (Heart of Courage) (Lyrics).mp3',
+    'Nightcore - Monster [NMV].mp3',
+    'Nightcore - PLAY x Unity x Faded  Alan Walker (Mashup  Switching Vocals) Lyrics.mp3',
+    'Nightcore - Sweet Little Bumblebee (lyric video).mp3',
+    'Nightcore - Thunder (Gabry Ponte, LUM!X, Prezioso) - (Lyrics).mp3',
+    'Nightcore - When You Leave (Numa Numa).mp3',
+    'Nightcore - Thunder (Gabry Ponte, LUM!X, Prezioso) - (Lyrics).mp3',
+    'Nightcore - Yo y los que tuvieron etapa Vocaloid cuando suena 😼✨.mp3',
+];
