709d9f9798558752cd21118e2fa2b19ad631832f
Archinime <archinime12@gmail.com>
2026-01-18 19:53:41 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
+/* Archivo: sw.js */
 const CACHE_NAME = 'archinime-cache-v1';
 const urlsToCache = [
   './',
   'index.html',
   'styles-index.css',
-  'Logo_Archinime.avif'
+  'Logo_Archinime.avif',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  // Obliga al SW a activarse inmediatamente
+  self.skipWaiting();
+  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  // Toma control de todos los clientes inmediatamente
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
