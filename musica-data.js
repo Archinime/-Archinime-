ff8e0896212dc2c6ce16cfb87d91446b4a08b0a8
Archinime <archinime12@gmail.com>
2026-01-20 16:50:32 -0500

    Update musica-data.js via CMS (New)

--- musica-data.js
+++ musica-data.js
@@ -241,5 +241,8 @@ const audioPlaylists = {
     ],
     "78": [
         "https://www.dropbox.com/scl/fi/fikl8mp1ht47xmzipjgxa/Hana-Kimi-Opening-Adrena-by-YOASOBI.mp3?rlkey=94ano4j50v8nhd492j9j2uw42&st=lnjja3mi&raw=1"
+    ],
+    "79": [
+        "https://www.dropbox.com/scl/fi/4ngz61g7k9s87o2rb2q3f/DARK-MOON-ENHYPEN-One-In-A-Billion-Japanese-Ver..mp3?rlkey=e8jj4bofrtrbi9gio0oqto2zc&st=lg79kept&raw=1"
     ]
 };
