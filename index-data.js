3049f7618319bdfd2510df220bfef461df86026f
Archinime <archinime12@gmail.com>
2026-01-22 09:01:05 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1177,8 +1177,8 @@ const animes = [
             "Fantasía",
             "Shōnen"
         ],
-        "lastUpdate": 1768946561991,
-        "updateType": "ESTRENO 🚨",
+        "lastUpdate": 1769090464954,
+        "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/68met98fq4br4u8cd7594/frieren2.avif?rlkey=w5jv3dne0ffs2ponw3ai1kgt0&st=uqyu2mmm&raw=1",
         "latestBlockName": "Temporada 2",
         "latestEpTitle": "Capítulo 1",
