b42aca664f6b6f15d594775ebe8818e37ebec029
Archinime <archinime12@gmail.com>
2026-01-26 11:17:04 -0500

    Update video-player-data.js via CMS (Edit)

--- video-player-data.js
+++ video-player-data.js
@@ -11075,6 +11075,11 @@ const players = {
                 "link": "https://drive.google.com/file/d/15XSgwYDGX-7K9_FeixnlVBfSEYnO9tsx/preview",
                 "link2": "",
                 "title": "Hana-Kimi T1 Cap 4"
+            },
+            "5": {
+                "link": "https://drive.google.com/file/d/1YARXfQWU20-k93AP99uPA_dlQZS6HqFh/preview",
+                "link2": "",
+                "title": "Hana-Kimi T1 Cap 5"
             }
         }
     },
