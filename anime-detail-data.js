2aee73133f40cb60caa8c21532004fc0adbfe289
Archinime <archinime12@gmail.com>
2026-01-27 10:51:08 -0500

    Update anime-detail-data.js via CMS (New)

--- anime-detail-data.js
+++ anime-detail-data.js
@@ -9040,5 +9040,112 @@ const data = {
                 "name": "Temporada 3"
             }
         ]
+    },
+    "84": {
+        "title": "Kobayashi-san Chi no Maid Dragon",
+        "desc": "Miss Kobayashi es la oficinista media con una vida aburrida y que vive en un pequeño apartamento sola. Esto era hasta que salva la vida a una dragón llamada Tohru con la capacidad de transformarse en una adorable chica humana. ¡Tohru hasta cualquier cosa que le sea posible para agradecer a Kobayashi que la salvara, quiera ella o no! Así, la vida de la oficinista pasa a complicarse por tener a una compañera de piso dragón.",
+        "cover": "https://api.pcloud.com/getpubthumb?code=XZ4jhK5ZV96vwk3dhrfuqylIjMK0SkhV0pyV&size=2000x3000",
+        "uploader": "archinime12@gmail.com",
+        "seasons": [
+            {
+                "num": 1,
+                "cover": "https://api.pcloud.com/getpubthumb?code=XZFcjK5ZpO8UyE7UJgJ1xEmweVpOd0fOYQ1y&size=2000x3000",
+                "eps": [
+                    {
+                        "title": "Capítulo 1"
+                    },
+                    {
+                        "title": "Capítulo 2"
+                    },
+                    {
+                        "title": "Capítulo 3"
+                    },
+                    {
+                        "title": "Capítulo 4"
+                    },
+                    {
+                        "title": "Capítulo 5"
+                    },
+                    {
+                        "title": "Capítulo 6"
+                    },
+                    {
+                        "title": "Capítulo 7"
+                    },
+                    {
+                        "title": "Capítulo 8"
+                    },
+                    {
+                        "title": "Capítulo 9"
+                    },
+                    {
+                        "title": "Capítulo 10"
+                    },
+                    {
+                        "title": "Capítulo 11"
+                    },
+                    {
+                        "title": "Capítulo 12"
+                    },
+                    {
+                        "title": "Capítulo 13"
+                    }
+                ],
+                "name": "Temporada 1"
+            },
+            {
+                "num": 2,
+                "cover": "https://api.pcloud.com/getpubthumb?code=XZjcjK5ZqUyWMqb24UQnImkXria4Fzpq51IV&size=2000x3000",
+                "eps": [
+                    {
+                        "title": "Capítulo 1"
+                    },
+                    {
+                        "title": "Capítulo 2"
+                    },
+                    {
+                        "title": "Capítulo 3"
+                    },
+                    {
+                        "title": "Capítulo 4"
+                    },
+                    {
+                        "title": "Capítulo 5"
+                    },
+                    {
+                        "title": "Capítulo 6"
+                    },
+                    {
+                        "title": "Capítulo 7"
+                    },
+                    {
+                        "title": "Capítulo 8"
+                    },
+                    {
+                        "title": "Capítulo 9"
+                    },
+                    {
+                        "title": "Capítulo 10"
+                    },
+                    {
+                        "title": "Capítulo 11"
+                    },
+                    {
+                        "title": "Capítulo 12"
+                    }
+                ],
+                "name": "Temporada 2"
+            },
+            {
+                "num": 3,
+                "cover": "https://api.pcloud.com/getpubthumb?code=XZjojK5Z0gt6QP9JFsB8waiQNJy5hFX2fF77&size=2000x3000",
+                "eps": [
+                    {
+                        "title": "A Lonely Dragon Wants to Be Loved"
+                    }
+                ],
+                "name": "Película 1"
+            }
+        ]
     }
 };
