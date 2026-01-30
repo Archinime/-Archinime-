8c4966be0fac28f53d1ec8985a70d00c4e664eae
Archinime <archinime12@gmail.com>
2026-01-29 23:33:26 -0500

    Update anime-detail-data.js via CMS (New)

--- anime-detail-data.js
+++ anime-detail-data.js
@@ -9563,5 +9563,98 @@ const data = {
                 "name": "Película 3"
             }
         ]
+    },
+    "86": {
+        "title": "Baki",
+        "desc": "Baki Hanma, un joven prodigio de las artes marciales que entrena incansablemente para superar a su padre, Yujiro Hanma, considerado el ser más fuerte del mundo, enfrentándose a luchadores extremadamente poderosos en peleas clandestinas y torneos para probar su fuerza y superar sus límites, con el objetivo final de derrotar a su padre.",
+        "cover": "https://api.pcloud.com/getpubthumb?code=XZ0p9K5Zz7CYJaaP8E4a4o9sBzCkrSwHI6k0&size=2000x3000",
+        "uploader": "archinime12@gmail.com",
+        "seasons": [
+            {
+                "num": 1,
+                "cover": "https://api.pcloud.com/getpubthumb?code=XZ9p9K5ZqjqTJUccGufTIcc4gX2Y0bamjI3k&size=2000x3000",
+                "eps": [
+                    {
+                        "title": "Capítulo 1"
+                    },
+                    {
+                        "title": "Capítulo 2"
+                    },
+                    {
+                        "title": "Capítulo 3"
+                    },
+                    {
+                        "title": "Capítulo 4"
+                    },
+                    {
+                        "title": "Capítulo 5"
+                    },
+                    {
+                        "title": "Capítulo 6"
+                    },
+                    {
+                        "title": "Capítulo 7"
+                    },
+                    {
+                        "title": "Capítulo 8"
+                    },
+                    {
+                        "title": "Capítulo 9"
+                    },
+                    {
+                        "title": "Capítulo 10"
+                    },
+                    {
+                        "title": "Capítulo 11"
+                    },
+                    {
+                        "title": "Capítulo 12"
+                    },
+                    {
+                        "title": "Capítulo 13"
+                    },
+                    {
+                        "title": "Capítulo 14"
+                    },
+                    {
+                        "title": "Capítulo 15"
+                    },
+                    {
+                        "title": "Capítulo 16"
+                    },
+                    {
+                        "title": "Capítulo 17"
+                    },
+                    {
+                        "title": "Capítulo 18"
+                    },
+                    {
+                        "title": "Capítulo 19"
+                    },
+                    {
+                        "title": "Capítulo 20"
+                    },
+                    {
+                        "title": "Capítulo 21"
+                    },
+                    {
+                        "title": "Capítulo 22"
+                    },
+                    {
+                        "title": "Capítulo 23"
+                    },
+                    {
+                        "title": "Capítulo 24"
+                    },
+                    {
+                        "title": "Capítulo 25"
+                    },
+                    {
+                        "title": "Capítulo 26"
+                    }
+                ],
+                "name": "Temporada 1"
+            }
+        ]
     }
 };
