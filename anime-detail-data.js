0df112f84533708b4b35066af78ec57416f77676
Archinime <archinime12@gmail.com>
2026-01-29 20:10:34 -0500

    Update anime-detail-data.js via CMS (Edit)

--- anime-detail-data.js
+++ anime-detail-data.js
@@ -7676,6 +7676,9 @@ const data = {
                     },
                     {
                         "title": "Capítulo 10"
+                    },
+                    {
+                        "title": "Capítulo 11"
                     }
                 ],
                 "name": "Temporada 1"
