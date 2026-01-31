1289542e9c030bb052a84cb900de78a0737b4f5c
Archinime <archinime12@gmail.com>
2026-01-31 15:00:44 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -950,11 +950,11 @@ const animes = [
             "Tokusatsu",
             "Seinen"
         ],
-        "lastUpdate": 1769402876047,
+        "lastUpdate": 1769889644015,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/1acwpuwtsx2f20urv44eo/Airbrush-IMAGE-ENHANCER-1763730194883-1763730194884.jpg?rlkey=ornue772yaoazcagrsinqj1fc&st=nlz517kl&raw=1",
         "latestBlockName": "Temporada 1",
-        "latestEpTitle": "Capítulo 13",
+        "latestEpTitle": "Capítulo 14",
         "isFinal": false,
         "aliases": [
             "Tojima Wants to Be a Kamen Rider"
