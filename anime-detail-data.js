f5e45677b44b0a33f923ab0506f44baa39e748f3
Archinime <archinime12@gmail.com>
2026-02-02 16:38:57 -0500

    Update anime-detail-data.js via CMS (Edit)

--- anime-detail-data.js
+++ anime-detail-data.js
@@ -7892,7 +7892,7 @@ const data = {
     },
     "72": {
         "title": "Seihantai na Kimi to Boku",
-        "desc": "Miyu Suzuki y Yusuke Tani son dos estudiantes de preparatoria totalmente opuestos: Suzuki es una chica enérgica y extrovertida que tiende a seguir a la multitud, incluso cuando no le apetece, mientras que Tani es un chico tranquilo e introvertido que dice lo que piensa sin rodeos.",
+        "desc": "Miyu Suzuki y Yusuke Tani son dos estudiantes de preparatoria totalmente opuestos: Suzuki es una chica enérgica y extrovertida que tiende a seguir a la multitud, incluso cuando no le apetece, mientras que Tani es un chico tranquilo e introvertido que dice lo que piensa sin rodeos",
         "cover": "https://www.dropbox.com/scl/fi/tj525d996dhx67ybfo85f/ImageToStl.com_MV5BNGI4NzhiZmMtNWY2Mi00MTFhLTgyOTktZTk1YjRlZmU5NzkxXkEyXkFqcGc._V1_FMjpg_UX1000.avif?rlkey=3so7gb70li05txtrk86ppisen&st=thjnvset&raw=1",
         "uploader": "archinime12@gmail.com",
         "seasons": [
