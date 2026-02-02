fde4210ad242dd7e50851a1f08c47ffe41fd6822
Archinime <archinime12@gmail.com>
2026-02-02 11:40:38 -0500

    Update musica-data.js via CMS (New)

--- musica-data.js
+++ musica-data.js
@@ -281,5 +281,9 @@ const audioPlaylists = {
         "https://www.dropbox.com/scl/fi/g24d8bw9nv0vueg4yjraa/Shangri-la-Frontier-Season-2-Opening-2-Frontiers.mp3?rlkey=zonzeec8wap1cnfjy3e6b42ug&st=ord0ivng&raw=1",
         "https://www.dropbox.com/scl/fi/gi53jsx8hz2eelzl3l15u/Shangri-la-Frontier-Opening-Broken-Games.mp3?rlkey=jbpo19u54pgbppdw0iat99h5i&st=t0m0vbxi&raw=1",
         "https://www.dropbox.com/scl/fi/6q2yn5h8ap1buqtwtebpg/Shangri-la-Frontier-Season-2-Opening-Queen-by-LiSA.mp3?rlkey=qysjemi1tkbl2oci6vf6k42hy&st=r7kk105w&raw=1"
+    ],
+    "91": [
+        "https://www.dropbox.com/scl/fi/1y2wdh5m8py2tx4bod0z0/TRIGUN-STAMPEDE-OP.-Full-TOMBI-Sub.-Espa-ol-AMV.mp3?rlkey=eawwgvqac9i4rq29b19mqodi9&st=r9zvbxcw&raw=1",
+        "https://www.dropbox.com/scl/fi/xuvi2n4vagouh77jrhke5/Eng-LyricsConcept-Art.VerTRIGUN-STAMPEDE-Full-Ending-by-Salyu-Haruka-Nakamura.mp3?rlkey=2go145cxrv62p3a9bej8wz3aw&st=8q4x78uq&raw=1"
     ]
 };
