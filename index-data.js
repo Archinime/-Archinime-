969f7eaaa39b74bd43a141ae6cdc9750a5fa3603
Archinime <archinime12@gmail.com>
2026-02-21 17:53:03 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1191,11 +1191,11 @@ const animes = [
             "Fantasía",
             "Shōnen"
         ],
-        "lastUpdate": 1771331943637,
+        "lastUpdate": 1771714381242,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/68met98fq4br4u8cd7594/frieren2.avif?rlkey=w5jv3dne0ffs2ponw3ai1kgt0&st=uqyu2mmm&raw=1",
         "latestBlockName": "Temporada 2",
-        "latestEpTitle": "Capítulo 2",
+        "latestEpTitle": "Capítulo 3",
         "isFinal": false,
         "aliases": [
             "Sōsō no Frieren"
