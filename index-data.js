261827a4358935283552a105dc629164d5179b66
Archinime <archinime12@gmail.com>
2026-02-27 09:12:30 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -35,11 +35,11 @@ const animes = [
             "Sobrenatural",
             "Shōnen"
         ],
-        "lastUpdate": 1771594235827,
+        "lastUpdate": 1772201550160,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/fsaecbkjhqot7rnqy2lnl/sfsf.avif?rlkey=xg05r2gx62t7jn2z5m9q47ehm&st=5azi7pqp&raw=1",
         "latestBlockName": "Temporada 3",
-        "latestEpTitle": "Capítulo 5",
+        "latestEpTitle": "Capítulo 6",
         "isFinal": false
     },
     {
