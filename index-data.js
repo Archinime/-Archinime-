d599633a12978713a89682509e8435a2c9b8cfbd
Archinime <archinime12@gmail.com>
2026-02-27 09:10:13 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1341,11 +1341,11 @@ const animes = [
             "Fantasía oscura",
             "Seinen"
         ],
-        "lastUpdate": 1771592852542,
+        "lastUpdate": 1772201412905,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/g6gmcpr341z0eav70s8xa/C2P4K6XQ5BBNBAKHMAM5XBFAKE.avif?rlkey=8q1u5gev6iq55ndsts7m6s5ij&st=x46rl6r1&raw=1",
         "latestBlockName": "Temporada 1",
-        "latestEpTitle": "Capítulo 7",
+        "latestEpTitle": "Capítulo 8",
         "isFinal": false,
         "aliases": [
             "Yuusha-kei ni Shosu"
