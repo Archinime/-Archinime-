621b80cc4420d789daf513e32f9b7f1ac8d8cd40
Archinime <archinime12@gmail.com>
2026-03-05 19:30:37 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1367,11 +1367,11 @@ const animes = [
             "Thriller",
             "Seinen"
         ],
-        "lastUpdate": 1772111193211,
+        "lastUpdate": 1772757035889,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/lxizxk8vjgtw98rlqs0gs/ImageToStl.com_MV5BMjhjNDQwYzEtY2EzNy00Zjk2LThmMjgtNGI2ODdlZDAxYzllXkEyXkFqcGc-._V1.avif?rlkey=evoiqoezadb87f7hqfqufabvg&st=qfm4dc84&raw=1",
         "latestBlockName": "Temporada 1",
-        "latestEpTitle": "Capítulo 8",
+        "latestEpTitle": "Capítulo 9",
         "isFinal": false,
         "aliases": [
             "Shibou Yuugi de Meshi wo Kuu"
