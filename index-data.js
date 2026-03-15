49118e8486c4d5d102c4f624a020b46383e46b7d
Archinime <archinime12@gmail.com>
2026-03-14 20:44:34 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1446,11 +1446,11 @@ const animes = [
             "Sobrenatural",
             "Shōnen"
         ],
-        "lastUpdate": 1772856203638,
+        "lastUpdate": 1773539072846,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/1vev0mnfqzgdkb64yehck/fireforce3.avif?rlkey=26ytp6sdp69ilxsr5wcxwifah&st=rnme9z8u&raw=1",
         "latestBlockName": "Temporada 3",
-        "latestEpTitle": "Capítulo 21",
+        "latestEpTitle": "Capítulo 22",
         "isFinal": false
     },
     {
