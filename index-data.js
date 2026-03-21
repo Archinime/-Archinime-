4577df97dade26959c788a4791075e5d17105fb5
Archinime <archinime12@gmail.com>
2026-03-20 23:52:32 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1494,11 +1494,11 @@ const animes = [
             "Sobrenatural",
             "Shōjo"
         ],
-        "lastUpdate": 1773538942915,
+        "lastUpdate": 1774068751943,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/2ji05um3swkxlqysdr70h/ImageToStl.com_Airbrush-IMAGE-ENHANCER-1768945509924-1768945509924.avif?rlkey=5cvtzsgy67fktx4om4qrphlco&st=ue9uf0aq&raw=1",
         "latestBlockName": "Temporada 1",
-        "latestEpTitle": "Capítulo 10",
+        "latestEpTitle": "Capítulo 11",
         "isFinal": false
     },
     {
