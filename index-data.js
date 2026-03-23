063780eca87abe4a96bdef4ed533d039f5d747aa
Archinime <archinime12@gmail.com>
2026-03-22 19:00:48 -0500

    Update index-data.js via CMS (Edit)

--- index-data.js
+++ index-data.js
@@ -1335,11 +1335,11 @@ const animes = [
             "Slice of Life",
             "Shōnen"
         ],
-        "lastUpdate": 1773597201550,
+        "lastUpdate": 1774224047668,
         "updateType": "NUEVO 🔥",
         "latestSeasonCover": "https://www.dropbox.com/scl/fi/87mra8kmnrk51w9uxoxc2/ImageToStl.com_v3_top_fv_kv02.avif?rlkey=yjbztlp5l7fjxsjuke5ffijf3&st=0iob3l2k&raw=1",
         "latestBlockName": "Temporada 1",
-        "latestEpTitle": "Capítulo 8",
+        "latestEpTitle": "Capítulo 9",
         "isFinal": false,
         "aliases": [
             "You And I Are Polar Opposites"
