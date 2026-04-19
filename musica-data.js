927beaada1141148331911aaa4422c285f681d82
Archinime <archinime12@gmail.com>
2026-04-19 09:11:30 -0500

    Update musica-data.js via CMS (Edit)

--- musica-data.js
+++ musica-data.js
@@ -189,8 +189,8 @@ const audioPlaylists = {
         "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/SaigoNiHitotsu.mp3"
     ],
     "61": [
-        "syn.mp3",
-        "syn2.mp3"
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/AttackOnTitan.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/AttackOnTitan1.mp3"
     ],
     "62": [
         "record.mp3"
