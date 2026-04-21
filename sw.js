6a5e9db70de5262ded89c2be5b48d91f531ff30a
Archinime <archinime12@gmail.com>
2026-04-21 12:11:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,10 +8,10 @@
    Compatible con PWA y notificaciones push (base)
    ============================================================ */
 
-const CACHE_STATIC = 'archinime-static-v17';
-const CACHE_DYNAMIC = 'archinime-dynamic-v17';
-const CACHE_IMAGES = 'archinime-images-v17';
-const CACHE_FONTS = 'archinime-fonts-v17';
+const CACHE_STATIC = 'archinime-static-v18';
+const CACHE_DYNAMIC = 'archinime-dynamic-v18';
+const CACHE_IMAGES = 'archinime-images-v18';
+const CACHE_FONTS = 'archinime-fonts-v18';
 
 // Recursos críticos a precachear (estáticos y siempre necesarios)
 const STATIC_ASSETS = [
