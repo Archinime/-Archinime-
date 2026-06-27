00cd0efdaaa73017ef648e994e9e84ea432fb3e2
Archinime <archinime12@gmail.com>
2026-06-27 16:06:18 -0500

    Add files via upload

--- gifs.js
+++ gifs.js
@@ -1,6 +1,6 @@
 // Lista de GIFs para la pantalla de carga
 const listaDeGifs = [
-  'chica_corriendo.gif',
+  'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/chica_corriendo.gif',
   'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/gokuu.gif',
   'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/naruto.gif'
 ];
