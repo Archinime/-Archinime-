dea816af2ffcdc8dc91b79d4723ae0cd31e94db7
Archinime <archinime12@gmail.com>
2026-07-10 21:00:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,10 +4,10 @@
    MEJORADO: Caché más inteligente, actualizaciones en caliente
    ============================================================ */
 
-const CACHE_STATIC = 'archinime-static-v70';
-const CACHE_DYNAMIC = 'archinime-dynamic-v70';
-const CACHE_IMAGES = 'archinime-images-v70';
-const CACHE_FONTS = 'archinime-fonts-v70';
+const CACHE_STATIC = 'archinime-static-v71';
+const CACHE_DYNAMIC = 'archinime-dynamic-v71';
+const CACHE_IMAGES = 'archinime-images-v71';
+const CACHE_FONTS = 'archinime-fonts-v71';
 
 const STATIC_ASSETS = [
   '/',
