728f1fecab58c767e3988554b80f9e5306344bce
Archinime <archinime12@gmail.com>
2026-07-12 13:16:58 -0500

    Add files via upload

--- musica_fondo.js
+++ musica_fondo.js
@@ -15,4 +15,5 @@ const musicListGlobal = [
   'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/Nightcore%20-%20When%20You%20Leave%20(Numa%20Numa).mp3',
   'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/Nightcore%20-%20Yo%20y%20los%20que%20tuvieron%20etapa%20Vocaloid%20cuando%20suena%20😼✨.mp3',
   'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/FLOW%20-%20HERO%20-Kibou%20no%20uta-.mp3',
+  'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/Caramella%20Girls.mp3',
 ];
